59f73e0c6e888a896d7c790e8fd46c6014d87e88
Will Mitchell <will@wmitchell.net>
2013-01-18 13:08:13 -0700

    Remove excess non-relevant comments

--- breakthrough/src/main.cpp
+++ breakthrough/src/main.cpp
@@ -8,18 +8,6 @@ using std::cout;
 
 int main(int argc, char* argv[])
 {
-    // Validate command line arguments
-
-    /*
-    // Start up server
-    GameState* gs = new GameState();
-    cout << *gs;
-    gs->get_moves();
-    GameState* gs2 = new GameState(7);
-    cout << *gs2;
-    GameState* gs3 = new GameState(4);
-    cout << *gs3;
-    */
 
     Server *s = new Server();
     s->play_game();
