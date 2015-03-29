deec11fb04a350d82102608f6b799717d639edd0
Will Mitchell <will@wmitchell.net>
2015-03-29 03:56:05 -0600

    Refactor board display to support click detection

--- GameMaster/gui/HexGameBoard.java
+++ GameMaster/gui/HexGameBoard.java
@@ -14,27 +14,59 @@ import java.util.ArrayList;
 import javax.swing.JPanel;
 import javax.swing.UIManager;
 
-// Leads to 24 * R = 320 * sqrt(3) = 554.25 height
-
 public class HexGameBoard extends AbstractBoardPanel implements MouseListener, MouseMotionListener {
   HexGameBoard() {
     setPreferredSize(
         new Dimension((int)(9 * FLAT_TO_FLAT + 2 * X_OFF),
                       (int)(52 * HALF_HEX_SIDE + 2 * Y_OFF))); // w, h
     addMouseListener(this);
+
+    createEmptyBoard();
   }
 
   @Override
   public void paintComponent(Graphics g) {
     super.paintComponent(g);
 
-    // All the drawing is on a BufferedImage, so we just need to draw the image
-    if (emptyBoard == null)
-      createBufferedImages();
+    Graphics2D g2d = (Graphics2D) g;
+    g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
-    g.drawImage(emptyBoard, 0, 0, null);
-    g.drawImage(piecesBoardOverlay, 0, 0, null);
+    // Clear existing
+    g2d.setColor(UIManager.getColor("Panel.background"));
+    g2d.fillRect(0, 0, getWidth(), getHeight());
 
+    // Draw information
+    g2d.setColor(UIManager.getColor("Panel.foreground"));
+    g2d.drawString("Turn: " + turn, 0, 20);
+
+
+    for (Spot i : state) {
+      switch(i.state) {
+      case EMPTY:
+        g2d.setPaint(Color.WHITE);
+        g2d.fill(i);
+        g2d.setStroke(new BasicStroke(3.0f));
+        g2d.setPaint(Color.BLACK);
+        g2d.drawOval(i.xx, i.yy, DIAMETER, DIAMETER);
+        break;
+      case PLAYER1:
+        g2d.setPaint(Color.RED);
+        g2d.fill(i);
+        g2d.setStroke(new BasicStroke(3.0f));
+        g2d.setPaint(Color.BLACK);
+        g2d.drawOval(i.xx, i.yy, DIAMETER, DIAMETER);
+        break;
+      case PLAYER2:
+        g2d.setPaint(Color.BLUE);
+        g2d.fill(i);
+        g2d.setStroke(new BasicStroke(3.0f));
+        g2d.setPaint(Color.BLACK);
+        g2d.drawOval(i.xx, i.yy, DIAMETER, DIAMETER);
+        break;
+      }
+    }
+
+    g2d.dispose();
   }
 
   @Override
@@ -43,11 +75,20 @@ public class HexGameBoard extends AbstractBoardPanel implements MouseListener, M
 
     // Split apart state
     String[] stateTokens = boardState.split(" ");
-    state.clear();
     try {
       // Start at 1 since first digit is player's turn
       for (int i = 1; i < stateTokens.length; ++i) {
-        state.add(Integer.parseInt(stateTokens[i]));
+        switch(Integer.parseInt(stateTokens[i])) {
+        case 0:
+          state.get(i - 1).state = SpotState.EMPTY;
+          break;
+        case 1:
+          state.get(i - 1).state = SpotState.PLAYER1;
+          break;
+        case 2:
+          state.get(i - 1).state = SpotState.PLAYER2;
+          break;
+        }
       }
     } catch (NumberFormatException e) {
       System.err.println("Invalid state string passed to HexGameBoard: " + boardState);
@@ -67,94 +108,23 @@ public class HexGameBoard extends AbstractBoardPanel implements MouseListener, M
       e.printStackTrace();
     }
 
-    drawBoard(piecesBoardOverlay);
-  }
-
-  private void drawBoard(BufferedImage bi) {
-    Graphics2D g2d = (Graphics2D) bi.getGraphics();
-    g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-
-    // Clear existing
-    g2d.setBackground(new Color(0, true));
-    g2d.clearRect(0, 0, bi.getWidth(), bi.getHeight());
-
-    // Draw information
-    g2d.setColor(UIManager.getColor("Panel.foreground"));
-    g2d.drawString("Turn: " + turn, 0, 20);
-
-    for (int i = 0; i < BOARD_DIM * BOARD_DIM; ++i) {
-      int row = i / BOARD_DIM;
-      int col = i % BOARD_DIM;
-      int dispRow = row + col;
-      int dispCol = col - row + BOARD_DIM - 1;
-
-      int currentVal = state.get(i);
-      if (currentVal == 0) {
-        // Empty spot
-        // Do nothing, underlying empty board takes care of it
-      } else if (currentVal == 1) {
-        // Player 1 piece
-        drawCircle(g2d, dispCol * COL_WIDTH, (int) (dispRow * ROW_HEIGHT), Color.RED);
-      } else if (currentVal == 2) {
-        // Player 2 piece
-        drawCircle(g2d, dispCol * COL_WIDTH, (int) (dispRow * ROW_HEIGHT), Color.BLUE);
-      }
-    }
-
-    g2d.dispose();
-
-    // Repaint to update
     repaint();
   }
 
-  private void drawCircle(Graphics2D g, int x, int y, Color c) {
-    g.setPaint(c);
-    g.fillOval(x + X_OFF, y + Y_OFF, DIAMETER, DIAMETER);
-    g.setStroke(new BasicStroke(3.0f));
-    g.setPaint(Color.BLACK);
-    g.drawOval(x + X_OFF, y + Y_OFF, DIAMETER, DIAMETER);
-  }
+  private void createEmptyBoard() {
+    state.clear();
 
-  private void createBufferedImages() {
-    emptyBoard = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_ARGB);
-    Graphics2D g2d = (Graphics2D) emptyBoard.getGraphics();
-    g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-
-    // Start with a clean slate
-    g2d.setColor(UIManager.getColor("Panel.background"));
-    g2d.fillRect(0, 0, getWidth(), getHeight());
-
-    // Draw all the pieces
     for (int i = 0; i < BOARD_DIM * BOARD_DIM; ++i) {
       int row = i / BOARD_DIM;
       int col = i % BOARD_DIM;
       int dispRow = row + col;
       int dispCol = col - row + BOARD_DIM - 1;
 
-      drawCircle(g2d, dispCol * COL_WIDTH, (int) (dispRow * ROW_HEIGHT), Color.WHITE);
+      Spot s = new Spot(i, X_OFF + dispCol * COL_WIDTH,
+                        Y_OFF + (int)(dispRow * ROW_HEIGHT), SpotState.EMPTY);
+      state.add(s);
     }
-
-    // Draw the grid upon which the board will be placed
-    // Really only need this uncommented if one wishes to understand the containing
-    // point code
-    for (int i = 0; i < 17; ++i) {
-      int topLine = (int) (i * ROW_HEIGHT) + Y_OFF;
-      int bottomLine = (int) ((i+1) * ROW_HEIGHT) + Y_OFF;
-      // Row lines
-      g2d.drawLine(X_OFF, topLine, getWidth(), topLine);
-      g2d.drawLine(X_OFF, bottomLine, getWidth(), bottomLine);
-      // Col lines
-      for (int j = 0; j < 9; ++j) {
-        int x = (2 * j + (i % 2)) * COL_WIDTH;
-        g2d.drawLine(x + X_OFF, topLine, x + X_OFF, bottomLine);
-      }
-    }
-
-    g2d.dispose();
-
-    piecesBoardOverlay = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_ARGB);
   }
-
 
   @Override
   public void mouseReleased(MouseEvent e) {
@@ -164,10 +134,22 @@ public class HexGameBoard extends AbstractBoardPanel implements MouseListener, M
   public void mouseClicked(MouseEvent e) {
     int x = e.getX() - X_OFF;
     int y = e.getY() - Y_OFF;
-    int boardRow = (int) ((double) y / ROW_HEIGHT);
-    int boardCol = (x / COL_WIDTH - (boardRow & 1)) / 2;
+    int dispCol = x / COL_WIDTH;
+    int dispRow = (int)(y / ROW_HEIGHT);
+    int col = (dispCol + dispRow - BOARD_DIM + 1) / 2;
+    int row = dispRow - col;
 
-    System.out.println("X = " + x + " Y = " + y + " boardRow = " + boardRow + " boardCol = " + boardCol + " (boardRow & 1) = " + (boardRow & 1));
+    // Outside of board
+    if (row < 0 || row >= BOARD_DIM || col < 0 || col >= BOARD_DIM)
+      return;
+
+    int idx = row * BOARD_DIM + col;
+
+    // Check actual click
+    if (!state.get(idx).contains(e.getPoint()))
+      return;
+
+    System.out.println("Clicked on " + idx);
   }
 
   @Override
@@ -200,6 +182,24 @@ public class HexGameBoard extends AbstractBoardPanel implements MouseListener, M
     }
   }
 
+  private enum SpotState {
+    EMPTY, PLAYER1, P1_MOVE_SOURCE, P1_MOVE_DEST,
+    PLAYER2, P2_MOVE_SOURCE, P2_MOVE_DEST }
+
+  private class Spot extends Ellipse2D.Double {
+    public int idx;
+    public int xx;
+    public int yy;
+    public SpotState state;
+    public Spot(int idx, int x, int y, SpotState state) {
+      super(x, y, DIAMETER, DIAMETER);
+      this.idx = idx;
+      this.xx = x;
+      this.yy = y;
+      this.state = state;
+    }
+  }
+
   private final static int FLAT_TO_FLAT = 40;
   private final static int COL_WIDTH = 20; // FLAT_TO_FLAT / 2
   private final static double HEX_SIDE = 23.1; // FLAT_TO_FLAT / sqrt(3)
@@ -210,10 +210,7 @@ public class HexGameBoard extends AbstractBoardPanel implements MouseListener, M
   private final static int Y_OFF = 5;
   private final static int BOARD_DIM = 9;
 
-  private BufferedImage emptyBoard;
-  private BufferedImage piecesBoardOverlay;
-
   private int turn = 0;
-  private ArrayList<Integer> state = new ArrayList<>();
   private ArrayList<Move> moves = new ArrayList<>();
+  private ArrayList<Spot> state = new ArrayList<>();
 }
